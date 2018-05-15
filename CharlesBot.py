e47c77028da2bd64e896301c4dc72b558748b06b
Jentlemen <gabriel8215@hotmail.com>
2018-05-14 20:54:49 -0400

    bot says rand "quote" from quotes.txt

--- CharlesBot.py
+++ CharlesBot.py
@@ -13,10 +13,22 @@ bot = commands.Bot(command_prefix = '~')
 async def ready():
     print("hello")
 
+#grabs quote from a file
+def grab_q():
+    q = open("quotes.txt", "r")
+    q_len = q.readline()
+    ran = random.randint(1,int(q_len))
+    quote = q.readline()
+    while True:
+        ran -=1 
+        if(not(ran)): return quote
+        else: quote = q.readline()
+        
 
+#bot will respond with a quote 
 @bot.command(pass_context=True)
 async def quote(cfx):
-    await bot.say("placeholder")
+    await bot.say(grab_q())
 
 tok = open("token.txt", "r")
 contents = tok.read()
