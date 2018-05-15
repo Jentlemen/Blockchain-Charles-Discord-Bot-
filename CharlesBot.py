bf0cac26500a88e53e6d5996d2abb4a74f189f62
Jentlemen <gabriel8215@hotmail.com>
2018-05-14 20:11:34 -0400

    Added Quote Command

--- CharlesBot.py
+++ CharlesBot.py
@@ -8,9 +8,15 @@ from discord.ext.commands import Bot
 
 bot = commands.Bot(command_prefix = '~')
 
+
 @bot.event 
 async def ready():
     print("hello")
+
+
+@bot.command(pass_context=True)
+async def quote(cfx):
+    await bot.say("placeholder")
 
 tok = open("token.txt", "r")
 contents = tok.read()
